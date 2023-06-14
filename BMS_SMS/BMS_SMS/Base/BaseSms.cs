a0603da80c10011d9c592eb738f546062f7e2e13
NewPanForMe <50812368+NewPanForMe@users.noreply.github.com>
2023-06-14 10:12:11 +0800

    新增发送短信库

---
+++ BMS_SMS/BMS_SMS/Base/BaseSms.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using AlibabaCloud.OpenApiClient.Models;
+using AlibabaCloud.SDK.Dysmsapi20170525;
+using AlibabaCloud.SDK.Dysmsapi20170525.Models;
+using BMS_SMS.Models;
+using Newtonsoft.Json;
+
+namespace BMS_SMS.Base
+{
+    public class BaseSms
+    {
+        public async void Send(string phoneNumber)
+        {
+            var config = new Config()
+            {
+                AccessKeyId = SmsBaseConfig.Instance.AccessKeyId,
+                AccessKeySecret = SmsBaseConfig.Instance.AccessKeySecret,
+                Endpoint = SmsBaseConfig.Instance.Endpoint
+            };
+            var client = new Client(config);
+            var sendSms = new SendSmsRequest()
+            {
+                PhoneNumbers = phoneNumber,
+                SignName = SmsSendConfig.Instance.SignName,
+                TemplateCode = SmsSendConfig.Instance.TemplateCode
+            };
+            var sendSmsResponse = await client.SendSmsAsync(sendSms);
+            Console.WriteLine(JsonConvert.SerializeObject(sendSmsResponse.Body));
+        }
+    }
+}
---
+++ BMS_SMS/BMS_SMS/Models/SmsConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BMS_SMS.Models
+{
+    public class SmsBaseConfig
+    { 
+        public static SmsBaseConfig Instance { get; set; }
+        /// <summary>
+        /// 密钥Id
+        /// </summary>
+        public string AccessKeyId { get; set; } = String.Empty;
+        /// <summary>
+        /// 密钥 Secret
+        /// </summary>
+        public string AccessKeySecret { get; set; } = String.Empty;
+        /// <summary>
+        /// 访问域名
+        /// </summary>
+        public string Endpoint { get; set; } = "dysmsapi.aliyuncs.com";
+
+
+    }
+    public class SmsSendConfig
+    {
+        public static SmsSendConfig Instance { get; set; }
+        /// <summary>
+        /// 密钥Id
+        /// </summary>
+        public string SignName { get; set; } = String.Empty;
+        /// <summary>
+        /// 密钥 Secret
+        /// </summary>
+        public string TemplateCode { get; set; } = String.Empty;
+    }
+}
